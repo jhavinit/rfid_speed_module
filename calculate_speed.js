871604a3b8f4fabac4ee0128920245f0b93bdbea
vinit <jhavinit07@gmail.com>
2019-10-15 23:45:47 +0530

    completed template for sending time :1/2 and printing speed

---
+++ calculate_speed.js
@@ -0,0 +1,32 @@
+var AWS = require("aws-sdk");
+AWS.config.update({
+  region: "us-east-2",
+    endpoint: "dynamodb.us-east-2.amazonaws.com",
+    accessKeyId: "AKIAJ2V62TAWOZ6MZE3Q",
+    secretAccessKey: "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"
+});
+var docClient = new AWS.DynamoDB.DocumentClient();
+var table = "speed_table";
+
+function read_from_dynamodb(uid){
+var params = {
+    TableName: table,
+    Key:{
+        "uid": uid
+        }
+};
+docClient.get(params, function(err, data) {
+    if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        if(!(JSON.stringify(data, null, 2).length == 2)){
+	  var t1 = data["Item"]["timestamp_reader_1"];
+	  var t2 = data["Item"]["timestamp_reader_2"];
+	  var distance = 30;
+	  console.log("Speed : ",distance/(t2 - t1));
+	}
+    }
+ }
+}
+read_from_dynamodb(1);
---
+++ send_timestamp_2.js
@@ -0,0 +1,49 @@
+var net = require('net');
+var AWS = require("aws-sdk");
+var HOST = '192.168.1.190'; //change
+var PORT = 6000;	//change
+var client = new net.Socket();
+
+AWS.config.update({
+  region: "us-east-2",
+    endpoint: "dynamodb.us-east-2.amazonaws.com",
+    accessKeyId: "AKIAJ2V62TAWOZ6MZE3Q",
+    secretAccessKey: "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"
+});
+var docClient = new AWS.DynamoDB.DocumentClient();
+var table = "speed_table";
+
+client.connect(PORT, HOST, function() {
+  console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+});
+
+function write_into_dynamodb(uid,t){
+var params = {
+    TableName:table,
+    Item:{
+        "uid": uid,
+        "timestamp_reader_2": t
+    }
+};
+console.log("Adding a new item...");
+docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", 
+JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+});
+}
+
+client.on('data', function(data) {
+  console.log('DATA: ' + data);
+  //convert the data into hex() string
+  //read t'
+  write_into_dynamodb(1,t);
+});
+
+//client.destroy();
+client.on('close', function() {
+  console.log('Connection closed');
+});
